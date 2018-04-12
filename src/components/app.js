59ccfb650994fcf1c6a8491986a7383d0c8f0b74
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-04-12 15:21:43 -0700

    add_sample

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,24 @@ export default class App extends Component {
   render() {
     return (
       <div>
+          <div className = 'tablePosition'>
+              <span className = 'cityTable'>
+          <table className = 'cityName'>
+              <tr>
+                  <th>Cities example: </th>
+            </tr>
+            <tr>
+                <td>New York</td>
+                <td>Los Angeles</td>
+            </tr>
+            <tr>
+                <td>Boston</td>
+                <td>Las Vegas</td>
+            </tr>
+
+          </table>
+      </span>
+      </div>
           <Alarm />
           <SearchBar />
           <WeatherList />
